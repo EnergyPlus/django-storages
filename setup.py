8bcc3b364e0bb072c42b3c4aeed848078b7885a7
Mikey Reppy <mike.reppy@nrg.com>
2017-06-05 15:47:38 -0400

    versioning for internal use

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_requirements_tests():
         return f.readlines()
 
 setup(
-    name='django-storages',
+    name='django-storages-internal',
     version=storages.__version__,
     packages=['storages', 'storages.backends'],
     author='Josh Schneier',
